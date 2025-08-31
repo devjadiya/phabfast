2687b438d7d5dcd7b73a4ab14ce63fd7673ff065
Dev Jadiya <devjadiya15@gmail.com>
2025-08-31 14:02:52 +0000

    Updated

--- src/app/api/tasks/search/route.ts
+++ src/app/api/tasks/search/route.ts
@@ -29,13 +29,30 @@ const projectPhidMap: Record<string, string[]> = {
     'bot-dev': [
         'PHID-PROJ-cfim5sbsz722ywm4ja6c', // Bots
         'PHID-PROJ-j2b5r2zvvxpfv4pyo5p7', // Pywikibot
-        'PHID-PROJ-w3f7swourh32qmdc6jgs'  // Toolforge
+        'PHID-PROJ-w3f7swourh32qmdc6jgs',  // Toolforge
+        'PHID-PROJ-b3aevxeyvrcx724i27u6', // Bot-Frameworks
+        'PHID-PROJ-qjddc42on3yqg5da2g43', // Cloud-Services
+        'PHID-PROJ-c46ybasbcjpfzaske7qh', // AutoWikiBrowser
     ],
-    'core': ['PHID-PROJ-evy6ry35h44jbgyz33mp'], // MediaWiki-Core
+    'core': [
+        'PHID-PROJ-evy6ry35h44jbgyz33mp', // MediaWiki-Core
+        'PHID-PROJ-c4ax6usbfzetj3f67i3c', // Action-API
+        'PHID-PROJ-uujzuqfajq327gnomrtf', // Database
+        'PHID-PROJ-iaak2h2rgyd7wz4vsqw2', // File-Backends
+        'PHID-PROJ-xwi6qyae7ndatdsvgvqc', // I18n-L10n
+        'PHID-PROJ-mswbzq3medraol2jhto2', // JobQueue
+        'PHID-PROJ-p7272t5v7jeihee3cb2m', // Logging
+        'PHID-PROJ-xck3caedubz2onjgya4x', // Skins
+        'PHID-PROJ-snjwnyaz2u5myo46j5n2', // Caching
+        'PHID-PROJ-nwvnhrgs2zwsx2u3b26y', // Security
+        'PHID-PROJ-sh7u7afy6v3oiprqge3f', // Performance
+    ],
     'gadgets': ['PHID-PROJ-ylt7fu5sxyuxb3xdrxdi'], // MediaWiki-Gadgets
     'web-tools': [
         'PHID-PROJ-w3f7swourh32qmdc6jgs', // Toolforge
-        'PHID-PROJ-qjddc42on3yqg5da2g43'  // Cloud-Services
+        'PHID-PROJ-qjddc42on3yqg5da2g43',  // Cloud-Services
+        'PHID-PROJ-ux524wbb2xizbgccjwfq', // Web-platform-team
+        'PHID-PROJ-yqnypxfdnzzmsoh6idja', // PAWS
     ],
 };
 
